4f7f7d9ee46173d2b97adf68478943ad41be814d
AttAditya <attachment.aditya@gmail.com>
2026-07-15 14:57:00 +0530

    feat: add reloads on persisted page shows
    Co-authored-by: HUN-sp <chopravinaykumarchopra@gmail.com>

--- portable/public/config-prod.js
+++ portable/public/config-prod.js
@@ -1,2 +1,8 @@
-window.apiBaseUrl = "https://delivr-be.attaditya.space";
+window.apiBaseUrl = "https://delivr-be.onrender.com";
+
+window.addEventListener("pageshow", (event) => {
+  if (event.persisted) {
+    window.location.reload();
+  }
+});
 
--- portable/public/config.js
+++ portable/public/config.js
@@ -1,2 +1,8 @@
 window.apiBaseUrl = "http://localhost:8000";
 
+window.addEventListener("pageshow", (event) => {
+  if (event.persisted) {
+    window.location.reload();
+  }
+});
+
