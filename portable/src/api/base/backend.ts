1e7ec2f7464e63d9e6e99617d0ebdb00de991a6f
AttAditya <attachment.aditya@gmail.com>
2026-07-16 14:15:12 +0530

    feat: better authorization headers
    Co-authored-by: HUN-sp <chopravinaykumarchopra@gmail.com>

--- portable/src/api/base/backend.ts
+++ portable/src/api/base/backend.ts
@@ -1,17 +1,16 @@
 import { ApiCaller } from "@api/base/api-caller";
 
-const baseApiUrl = (window as any).apiBaseUrl;
-const apiToken = localStorage.getItem("apiToken") || "Unauthorized";
-
 export const backendApi = new ApiCaller({
-  baseApiUrl,
+  baseApiUrl: (window as any).apiBaseUrl,
   credentials: "include",
   mode: "cors",
   retries: 3,
   defaultHeaders: {
     "Content-Type": "application/json",
-    "Authorization": `Bearer ${apiToken}`,
     "Accept": "application/json",
+    "Authorization": `Bearer ${
+      localStorage.getItem("apiToken") || "Unauthorized"
+    }`,
   },
 });
 
