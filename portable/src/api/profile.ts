d0e6bd57fafa7e8237fb9aa6e31c826a25b01e6d
AttAditya <attachment.aditya@gmail.com>
2026-08-01 16:54:58 +0530

    feat: remove ls caching for profile
    Co-authored-by: HUN-sp <chopravinaykumarchopra@gmail.com>

--- portable/src/api/profile.ts
+++ portable/src/api/profile.ts
@@ -1,16 +1,7 @@
 import { backendApi } from "@api/base/backend";
 
 export async function fetchProfile() {
-  const data = await backendApi.callApi(
-    "/users/me",
-    "GET",
-    undefined,
-    {},
-    {
-      lsCached: true,
-      lsCacheTTL: 60 * 1000,
-    },
-  );
+  const data = await backendApi.callApi("/users/me", "GET");
 
   return { data };
 }
